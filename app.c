868fb728d374d92fb3dee0773d301b0d34b94f81
Joaquim Manjama <melorafaelmanjama@gmail.com>
2025-12-08 02:41:51 +0000

    draw a pixel

--- app.c
+++ app.c
@@ -2,15 +2,34 @@
 #include "SDL.h"
 
 int main() {
+
+    // Create Window 
     int width = 800;
     int height = 800;
 
     SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
 
+    // Surface
+    SDL_Surface *psurface = NULL;
+    psurface = SDL_GetWindowSurface(pwindow);
+
+    // Pixel
+    SDL_Rect pixel;
+    pixel.x = 100;
+    pixel.y = 100;
+    pixel.w = 2;
+    pixel.h = 2;
+
+    // Draw Rect
+    SDL_FillRect(psurface, &pixel, 0xFF0000);
+
+    // Update Window
+    SDL_UpdateWindowSurface(pwindow);
+
     int running = 1;
-    //Event Loop
+    // Event Loop
     while (running) {
-        
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {  // poll until all events are handled!
             // decide what to do with this event.
@@ -21,6 +40,11 @@ int main() {
 
         // update game state, draw the current frame
     }
+
+    SDL_FreeSurface(psurface);
+    SDL_DestroyWindow(pwindow);
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
 
 /*  ****  TODO  **** 
